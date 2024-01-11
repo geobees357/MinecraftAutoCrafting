e4733f6755e3f81da66a0df3b9b7b43545c1cc14
Madeline Celestia <20229468+Meepster99@users.noreply.github.com>
2024-01-11 17:30:33 -0500

    morestuff

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ fucking dumb.
 --]]
 
 
-print("bruh?")
+--print("bruh?")
 
 Integrator = {}
 
@@ -24,7 +24,7 @@ function Integrator:pulse()
     self.set(self.defaultVal)
 end
 
-function Integrator:get()
+function Integrator:get() -- not actually needed anymore,, but yea
     assert(self.defaultVal == nil, "defaultVal was not nil, and needs to be for a input integrator!")
     return self.peripheral.getInput(self.side)
 end
@@ -50,20 +50,49 @@ function Integrator:new(peripheral, side, defaultVal)
     return obj
 end
 
-attackPiston = 
-usePiston = 
-deployerTrigger = 
+function Container:list()
+    return self.peripheral.list()
+end
 
-chest = 
-funnel = 
-outputChest = 
+function Container:size()
+    return self.size
+end
 
-returnHopper = 
-giveHopper = 
+function Container:new(peripheral)
+    
+    -- wtf?
+    local obj = {}
+    setmetatable(obj, self)
+    self.__index = self
+
+    -- define vars
+    self.peripheral = peripheral
+    self.size = peripheral.size()
+
+    return obj
+end
+
+
+attackPiston = Integrator:new(peripheral.wrap("redstoneIntegrator_11"), "back", false) -- piston for the attack deployer
+usePiston = Integrator:new(peripheral.wrap("redstoneIntegrator_7"), "back", false) -- piston for the use deployer
+
+deployerTrigger = Integrator:new(peripheral.wrap("redstoneIntegrator_10"), "bottom", true) -- redstone for triging the deployer
+
+funnel = Integrator:new(peripheral.wrap("redstoneIntegrator_9"), "bottom", false)  -- trigs the funnel
+outputBufferSend = Integrator:new(peripheral.wrap("redstoneIntegrator_8"), "front", true) -- trig the sending of item from output buffer
+
+
+recipeChest = Container:new(peripheral.wrap("barrel_2"))
+outputBuffer = Container:new(peripheral.wrap("barrel_1"))
+
+returnHopper = Container:new(peripheral.wrap("hopper_0"))
+giveHopper = Container:new(peripheral.wrap("hopper_1"))
 
 modem = peripheral.wrap("top")
 channel = 5
 modem.open(channel)
+
+checkSlotIDs = {1,2,3,10,11,12,19,20,21}
 
 function transmitMessage(message)
     modem.transmit(channel, channel, message)
@@ -75,7 +104,13 @@ function checkForMessageEvent()
 end
 
 
-
+--check items in choosen slots 
+function gridContents(slot)
+    local items = recipeChest.list()
+    for i=1
+        
+    end
+end
 
 
 
