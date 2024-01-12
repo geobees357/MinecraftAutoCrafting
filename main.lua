4d55be5806d813d0d9c752e71bf89e336177e919
Zachary <59158601+geobees357@users.noreply.github.com>
2024-01-11 19:46:19 -0800

    more more rmoe

--- main.lua
+++ main.lua
@@ -196,7 +196,7 @@ checkSlotIDs = {{1,10,19},{2,11,20},{3,12,21}}
 function trigDeployer(piston) -- trigs the deployer (assuming ones currently there)
     piston:set()
     deployerTrigger:pulse()
-    sleep(0.5) -- is this a good amount of time?
+    sleep(0.2) -- is this a good amount of time?
     piston:reset()
 end
 
@@ -212,7 +212,7 @@ function getItem(slot)
 
     funnel:pulse()
 
-    sleep(0.5)
+    sleep(0.1)
 
     local res = recipeBuffer:getItemDetail(1)
 
@@ -248,7 +248,7 @@ function craft()
 
     -- crafting will just,,, occur over here automatically??
 
-    sleep(3)
+    sleep(2)
 
     local res = outputBuffer:getItemDetail(1)
 
