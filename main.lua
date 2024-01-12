ab182bf3a4ed05917f56453703342c9030b8b982
Madeline Celestia <20229468+Meepster99@users.noreply.github.com>
2024-01-11 20:26:20 -0500

    ?

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@
 --[[
 
 goofy ahh computer waits for a redstone signal, GRABS THE GIT REPO, AND EXECS THE STRING AS CODE???!
-fucking dumb.
+fucking dumb. 
 when getting in the thing,,, make sure to have ? and then the time so things are unique and the cache doesnt fuck us?
 
 obj:func() is SO STUPID.
